2abb5169641635f1c0ad2efbe1d4df8aa4a2e0c1
Eduardo Pontes <eduardo.pontes2801@gmail.com>
2023-01-29 18:08:06 -0300

    fix/profileAndImages

--- src/App.js
+++ src/App.js
@@ -91,10 +91,8 @@ function App() {
             <div className="row">
               <div className="col-12">
                 <p>
-                I attended the higher education institution Universidade Católica de Brasília - UCB, experience and notions of
-programming in full stack development (client-side and server-side, frameworks and database). Active, always
-looking for the best way to deal and premeditate in several situations. I participate easily in teamwork helping to
-solve problems, communicative, agile and helpful.
+                Full-stack developer with a bachelor's degree in Computer Science from Universidade Católica de Brasília (UCB), experience in client-side and server-side development, frameworks and databases. Active, always looking for the best way to handle and premeditate in various situations. I participate easily in teamwork helping to solve problems, communicative, agile and helpful.
+
                 </p>
               </div>
             </div>
@@ -127,15 +125,38 @@ solve problems, communicative, agile and helpful.
               
               <div className="row">
                 <div className="col-12 text-center">
-                  <i className="fa-brands fa-js-square" title="JavaScript and TypeScript"></i>
+                  {/* <i className="fa-brands fa-js-square" title="JavaScript and TypeScript"></i>
                   <i className="fa-brands fa-node" title="Node.js"></i>
                   <i className="fa-brands fa-react" title="React.js"></i>
                   <i className="fa-brands fa-vuejs" title="VUE.js"></i>
                   <i className="fa-brands fa-java" title="Java"></i>
                   <i className="fa-brands fa-php" title="PHP"></i>
+                  <i className="fa-brands fa-laravel" title="Laravel"></i>
                   <i className="fa-brands fa-python" title="Python"></i>
                   <i className="fa-brands fa-docker" title="Docker"></i>
-                  <i className="fa-solid fa-database" title="MySQL and MongoDB"></i>
+                  <i className="fa-solid fa-database" title="MySQL and MongoDB"></i> */}
+                
+                  <a href="https://nodejs.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/nodejs-original-wordmark.svg" alt="Node.js" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://expressjs.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/express-original-wordmark.svg" alt="Express.js" height="40" style={{margin: 5, backgroundColor: 'white'}}/></a>  
+                  <a href="https://www.javascript.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/javascript-original.svg" alt="JavaScript" height="40" style={{margin: 5}}/></a>   
+                  <a href="https://www.typescriptlang.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/typescript-original.svg" alt="TypeScript" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://reactjs.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/react-original-wordmark.svg" alt="React" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://redux.js.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/redux-original.svg" alt="Redux" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://nextjs.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/nextjs.png" alt="NextJS" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://mui.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/mui.png" alt="Material UI" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://vuejs.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/vuejs-original-wordmark.svg" alt="Vue.js" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.tailwindcss.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/tailwindcss.svg" alt="Tailwind CSS" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.php.net/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/php-original.svg" alt="PHP" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://laravel.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/laravel-plain-wordmark.svg" alt="Laravel" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://symfony.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/symfony_black_03.svg" alt="Symfony" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://codeigniter.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/codeigniter.svg" alt="CodeIgniter" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.java.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/java-original-wordmark.svg" alt="Java" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.python.org/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/python-original.svg" alt="Python" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://flask.palletsprojects.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/flask.png" alt="Flask" height="40" style={{margin: 5, backgroundColor: 'white'}}/></a>  
+                  <a href="https://www.docker.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/docker-original-wordmark.svg" alt="Docker" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.nginx.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/nginx-original.svg" alt="Nginx" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.mongodb.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/mongodb-original-wordmark.svg" alt="MongoDB" height="40" style={{margin: 5}}/></a>  
+                  <a href="https://www.mysql.com/" target="_blank"><img src="https://profilinator.rishav.dev/skills-assets/mysql-original-wordmark.svg" alt="MySQL" height="40" style={{margin: 5}}/></a>  
                 </div>
               </div>
           </div>
